0d5f5e34eaf5de3d06d5dc1534224ec7ffad5028
Fatih Mutaf <fmutaf10@gmail.com>
2026-03-24 13:29:39 +0300

    yeni depo

--- varliklar/javascriptkod/script.js
+++ varliklar/javascriptkod/script.js
@@ -7,22 +7,4 @@ function kareAl(sayi) {
     let kare = sayi**2;
     return kare;
 }
-let sonuncuDeger = kareAl(sonDeger);
 
-function besEkle(sayi) {
-    let sonuc = sayi+5;
-    return sonuc;
-}
-let eklenmisDeger = besEkle(sonuncuDeger);
-
-function altiEkle(sayi) {
-    let sonuc = sayi+6;
-    return sonuc;
-}
-let nextDeger = altiEkle(eklenmisDeger);
-
-function onEkle(sayi) {
-    let sonuc = sayi+10;
-    console.log(sonuc);
-}
-let tamDeger = onEkle(nextDeger);
