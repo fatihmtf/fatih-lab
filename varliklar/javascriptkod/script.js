b9b69321ba5608c7270853d6a1a558d611a19488
Fatih Mutaf <fmutaf10@gmail.com>
2026-03-24 13:34:50 +0300

    üçüncü depo

--- varliklar/javascriptkod/script.js
+++ varliklar/javascriptkod/script.js
@@ -2,5 +2,25 @@ function ikiEkle(sayi) {
     let sonuc = sayi+2;
     return sonuc;
 }
+function ikiEkle(sayi) {
+    let sonuc = sayi+2;
+    return sonuc;
+}
+function ikiEkle(sayi) {
+    let sonuc = sayi+2;
+    return sonuc;
+}
+function ikiEkle(sayi) {
+    let sonuc = sayi+2;
+    return sonuc;
+}
+function ikiEkle(sayi) {
+    let sonuc = sayi+2;
+    return sonuc;
+}
+function ikiEkle(sayi) {
+    let sonuc = sayi+2;
+    return sonuc;
+}
 
 
